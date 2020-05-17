a5948fe39e4086dcc2a12edc3b4b5f2140a701c0
Assen Totin <assen.totin@gmail.com>
2020-05-18 00:03:22 +0300

    Turn off modules by default

--- src/modules.h
+++ src/modules.h
@@ -5,22 +5,22 @@
  */
 
 // To enable the JWT-based authorisation, uncomment the following line
-#define CDN_ENABLE_JWT 1
+//#define CDN_ENABLE_JWT 1
 
 // To enable the MongoDB transport and request, uncomment the following line
-#define CDN_ENABLE_MONGO 1
+//#define CDN_ENABLE_MONGO 1
 
 // To enable the MySQL transport and request, uncomment the following line
-#define CDN_ENABLE_MYSQL 1
+//#define CDN_ENABLE_MYSQL 1
 
 // To enable the Oracle transport and request, uncomment the following line
 //#define CDN_ENABLE_ORACLE 1
 
 // To enable the PostgreSQL transport and request, uncomment the following line
-#define CDN_ENABLE_POSTGRESQL 1
+//#define CDN_ENABLE_POSTGRESQL 1
 
 // To enable the Redis transport, uncomment the following line
-#define CDN_ENABLE_REDIS 1
+//#define CDN_ENABLE_REDIS 1
 
 
 
