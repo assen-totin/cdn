827b6e800194f594feafa4b72c92c1f3f03d78cc
Assen Totin <assen.totin@gmail.com>
2020-05-18 00:37:57 +0300

    Update build.

--- build-files/build.sh
+++ build-files/build.sh
@@ -66,31 +66,31 @@ pushd $CHECKOUT_DIR
 # Extra libraries needed by this modules
 EXTRA_LIBS="-lbson-1.0 -lcurl -lxml2"
 EXTRA_INCLUDES="-I /usr/include/libbson-1.0 -I/usr/include/libxml2"
-if [ x$ARG_CND_ENABLE_JWT != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_JWT != 'x' ] ; then
 	EXTRA_LIBS="$EXTRA_LIBS -ljwt"
 	sed -i 's|^.*CDN_ENABLE_JWT.*$|#define CDN_ENABLE_JWT|' src/modules.h
 fi
-if [ x$ARG_CND_ENABLE_MONGO != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_MONGO != 'x' ] ; then
 	EXTRA_LIBS="$EXTRA_LIBS -lmongoc-1.0"
 	EXTRA_INCLUDES="$EXTRA_INCLUDES -I /usr/include/libmongoc-1.0"
 	sed -i 's|^.*CDN_ENABLE_MONGO.*$|#define CDN_ENABLE_MONGO|' src/modules.h
 fi
-if [ x$ARG_CND_ENABLE_MYSQL != 'x' ] ; then
-	[ x$EL_VERSION == 'x7' ] && EXTRA_LIBS="$EXTRA_LIBS -L/usr/lib64/mysql"
+if [ x$ARG_CDN_ENABLE_MYSQL != 'x' ] ; then
+	[ x$EL_VERSION == 'xel7' ] && EXTRA_LIBS="$EXTRA_LIBS -L/usr/lib64/mysql"
 	EXTRA_LIBS="$EXTRA_LIBS -lmysqlclient"
 	EXTRA_INCLUDES="$EXTRA_INCLUDES -I /usr/include/mysql"
 	sed -i 's|^.*CDN_ENABLE_MYSQL.*$|#define CDN_ENABLE_MYSQL|' src/modules.h
 fi
-if [ x$ARG_CND_ENABLE_POSTGRESQL != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_POSTGRESQL != 'x' ] ; then
 	EXTRA_LIBS="$EXTRA_LIBS -lpq"
 	sed -i 's|^.*CDN_ENABLE_POSTGRESQL.*$|#define CDN_ENABLE_POSTGRESQL|' src/modules.h
 fi
-if [ x$ARG_CND_ENABLE_ORACLE != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_ORACLE != 'x' ] ; then
 	export LD_LIBRARY_PATH=$ARG_CDN_ENABLE_ORACLE_HOME/lib
 	EXTRA_LIBS="$EXTRA_LIBS -locilib"
 	sed -i 's|^.*CDN_ENABLE_ORACLE.*$|#define CDN_ENABLE_ORACLE|' src/modules.h
 fi
-if [ x$ARG_CND_ENABLE_REDIS != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_REDIS != 'x' ] ; then
 	EXTRA_INCLUDES="$EXTRA_INCLUDES -I /usr/include/hiredis"
 	EXTRA_LIBS="$EXTRA_LIBS -lhiredis"
 	sed -i 's|^.*CDN_ENABLE_REDIS.*$|#define CDN_ENABLE_REDIS|' src/modules.h
@@ -109,12 +109,12 @@ print_notice $EL_VERSION
 print_notice $EXTRA_INCLUDES
 print_notice $EXTRA_LIBS
 
-if [ x$EL_VERSION == 'x7' ] ; then
+if [ x$EL_VERSION == 'xel7' ] ; then
 	CFLAGS=-Wno-error ./configure --add-dynamic-module=../src --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/run/nginx.pid --lock-path=/run/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-ipv6 --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-stream_ssl_preread_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-http_auth_request_module --with-mail=dynamic --with-mail_ssl_module --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_ssl_module --with-google_perftools_module --with-debug --with-cc-opt="-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -m64 -mtune=generic $EXTRA_INCLUDES" --with-ld-opt="-Wl,-z,relro -specs=/usr/lib/rpm/redhat/redhat-hardened-ld -Wl,-E $EXTRA_LIBS"
 fi
 
 # For EL8, we need to apply one of the patches from the source RPM first or code will not build
-if [ x$EL_VERSION == 'x8' ] ; then
+if [ x$EL_VERSION == 'xel8' ] ; then
 	patch -p1 < ../patches/0001*
 	CFLAGS=-Wno-error ./configure --add-dynamic-module=../src --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/run/nginx.pid --lock-path=/run/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-ipv6 --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-http_auth_request_module --with-mail=dynamic --with-mail_ssl_module --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_ssl_module --with-debug --with-cc-opt="-O2 -g -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fexceptions -fstack-protector-strong -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection $EXTRA_INCLUDES" --with-ld-opt="-Wl,-z,relro -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld -Wl,-E $EXTRA_LIBS"
 fi
@@ -136,39 +136,39 @@ cp -r $CHECKOUT_DIR/tools $RPM_HOME/SOURCES
 copy_spec_file
 
 # Update RPM dependencies
-if [ x$ARG_CND_ENABLE_JWT != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_JWT != 'x' ] ; then
 	sed -i 's|^.*libjwt-devel.*$|BuildRequires: libjwt-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	sed -i 's|^.*libjwt$|Requires: libjwt|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 fi
-if [ x$ARG_CND_ENABLE_MONGO != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_MONGO != 'x' ] ; then
 	sed -i 's|^.*mongo-c-driver-devel.*$|BuildRequires: mongo-c-driver-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	sed -i 's|^.*mongo-c-driver$|Requires: mongo-c-driver|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 fi
-if [ x$ARG_CND_ENABLE_MYSQL != 'x' ] ; then
-	if [ x$EL_VERSION == 'x7' ] ; then
+if [ x$ARG_CDN_ENABLE_MYSQL != 'x' ] ; then
+	if [ x$EL_VERSION == 'xel7' ] ; then
 		sed -i 's|^.*mariadb-devel.*$|BuildRequires: mariadb-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 		sed -i 's|^.*mariadb-libs.*$|Requires: mariadb-libs|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	fi
-	if [ x$EL_VERSION == 'x8' ] ; then
+	if [ x$EL_VERSION == 'xel8' ] ; then
 		sed -i 's|^.*BuildRequires: mariadb-connector-c-devel.*$|BuildRequires: mariadb-connector-c-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 		sed -i 's|^.*Requires: mariadb-connector-c-devel.*$|Requires: mariadb-connector-c-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	fi
 fi
-if [ x$ARG_CND_ENABLE_POSTGRESQL != 'x' ] ; then
-	if [ x$EL_VERSION == 'x7' ] ; then
+if [ x$ARG_CDN_ENABLE_POSTGRESQL != 'x' ] ; then
+	if [ x$EL_VERSION == 'xel7' ] ; then
 		sed -i 's|^.*BuildRequires: postgresql-devel.*$|BuildRequires: postgresql-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 		sed -i 's|^.*Requires: postgresql.*$|Requires: postgresql|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	fi
-	if [ x$EL_VERSION == 'x8' ] ; then
+	if [ x$EL_VERSION == 'xel8' ] ; then
 		sed -i 's|^.*BuildRequires: libpq-devel.*$|BuildRequires: libpq-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 		sed -i 's|^.*Requires: libpq.*$|Requires: libpq|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	fi
 fi
-if [ x$ARG_CND_ENABLE_ORACLE != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_ORACLE != 'x' ] ; then
 	sed -i 's|^.*ocilib-devel.*$|BuildRequires: ocilib-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	sed -i 's|^.*ocilib$|Requires: ocilib|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 fi
-if [ x$ARG_CND_ENABLE_REDIS != 'x' ] ; then
+if [ x$ARG_CDN_ENABLE_REDIS != 'x' ] ; then
 	sed -i 's|^.*hiredis-devel.*$|BuildRequires: hiredis-devel|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 	sed -i 's|^.*Requires: hiredis.$|Requires: hiredis|' $RPM_HOME/SPECS/$RPM_PACKAGE.spec
 fi
