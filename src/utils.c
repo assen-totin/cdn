06ca4ec5e82753bfffe7330273aff3721633d56e
Assen Totin <assen.totin@gmail.com>
2022-07-29 00:26:08 +0300

    Separate globals

--- src/utils.c
+++ src/utils.c
@@ -12,7 +12,7 @@
 #include "cache.h"
 
 // We need this here as a declaration only; it is defined in main header file which will resolve it at runtime.
-ngx_module_t ngx_http_cdn_module;
+extern ngx_module_t ngx_http_cdn_module;
 
 /**
  * Polyfill for memstr()
