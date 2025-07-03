13e9b88ae466c884c852df4ad7c61131acf6f73e
Assen Totin <assen.totin@curaden.ch>
2025-07-03 19:24:41 +0300

    Build

--- src/request.h
+++ src/request.h
@@ -20,6 +20,7 @@ ngx_int_t response_post_mysql(session_t *session, metadata_t *metadata, ngx_http
 
 // None
 ngx_int_t request_get_none(session_t *session, metadata_t *metadata, ngx_http_request_t *r);
+ngx_int_t request_post_none(session_t *session, metadata_t *metadata, ngx_http_request_t *r);
 ngx_int_t response_get_none(session_t *session, metadata_t *metadata, ngx_http_request_t *r);
 ngx_int_t response_post_none(session_t *session, metadata_t *metadata, ngx_http_request_t *r);
 
