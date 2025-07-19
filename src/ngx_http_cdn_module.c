d9578ffbf9162f21dc035f63ae5f8d63bab08384
Assen Totin <assen.totin@gmail.com>
2025-07-19 11:10:38 +0300

    Test Nginx 1.26

--- src/ngx_http_cdn_module.c
+++ src/ngx_http_cdn_module.c
@@ -97,7 +97,9 @@ void ngx_http_cdn_module_end(ngx_cycle_t *cycle) {
 #endif
 
 	// Clean all instances
-	for (i=0; i < globals->instances_cnt; i++) {
+cdn_debug("globals->instances_cnt: %i", globals->instances_cnt);
+//	for (i=0; i < globals->instances_cnt; i++) {
+	for (i=1; i < globals->instances_cnt; i++) {
 		instance = &globals->instances[i];
 
 		cache_destroy(instance->cache);
