c6a105935452520e53ca8b20ed5fbc232e3adecb
Assen Totin <assen.totin@gmail.com>
2025-07-19 10:31:44 +0300

    Test Nginx 1.26

--- src/cache.c
+++ src/cache.c
@@ -6,6 +6,7 @@
 
 #include "common.h"
 #include "globals.h"
+#include "utils.h"
 
 // Init BTree mask
 static uint64_t *init_btree_mask() {
@@ -293,20 +294,22 @@ static void btree_purge(btree_t *node, int level) {
 }
 
 void cache_destroy(cache_t *cache) {
+cdn_debug("Breakpoint: %s", "a");
 	if (! cache)
 		return;
-
+cdn_debug("Breakpoint: %s", "b");
 	if (cache->root) {
 		btree_purge(cache->root, 0);
 		free(cache->root);
 	}
-
+cdn_debug("Breakpoint: %s", "c");
 	if (cache->btree_mask)
 		free(cache->btree_mask);
-
+cdn_debug("Breakpoint: %s", "d");
 	if (cache->list)
 		free(cache->list);
-
+cdn_debug("Breakpoint: %s", "e");
 	free(cache);
+cdn_debug("Breakpoint: %s", "f");
 }
 
