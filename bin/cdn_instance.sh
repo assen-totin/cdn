d6dc941ef49970912b90649b9fbfcaac67ba7027
Assen Totin <assen.totin@gmail.com>
2026-02-17 10:58:21 +0200

    Full log

--- bin/cdn_instance.sh
+++ bin/cdn_instance.sh
@@ -42,9 +42,11 @@ for CONFIG_FILE in $CONFIG_FILES ; do
 	done
 
 	# Chown/chmod the file if configured
-	[ "x$FS_USER" != 'x' ] && chown $FS_USER $FULL_LOG
-	[ "x$FS_GROUP" != 'x' ] && chgrp $FS_GROUP $FULL_LOG
-	[ "x$FS_MODE" != 'x' ] && chmod $FS_MODE $FULL_LOG
+	if [ -f $FULL_LOG ] ; then
+		[ "x$FS_USER" != 'x' ] && chown $FS_USER $FULL_LOG
+		[ "x$FS_GROUP" != 'x' ] && chgrp $FS_GROUP $FULL_LOG
+		[ "x$FS_MODE" != 'x' ] && chmod $FS_MODE $FULL_LOG
+	fi
 
 	# Cleanup: remove all files older than $KEEP days
 	find -type f -mtime +$KEEP | xargs rm -f
